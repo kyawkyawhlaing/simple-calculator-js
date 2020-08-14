39b888c018ca8d5fc15676b6448561b84494a58d
kyawkyawhlaing <kyawkyawhlaing1997@gmail.com>
2020-08-14 23:06:55 +0630

    upload

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
-var operands = document.querySelectorAll("#number");
-var operators = document.querySelectorAll("#operator");
-
-operands.forEach((operand) => {
-    operand.addEventListener(
-        'click', function () {
-            document.querySelector("#history-value").innerHTML = Number(operand.textContent);
-        }
-    )
-})
+//function for displaying values
+function dis(val) {
+    document.getElementById("data").value += val;
+}
+//function for evaluation
+function solve() {
+    let x = document.getElementById("data").value;
+    let y = eval(x);
+    document.getElementById("data").value = y;
+}
+//function for clearing the display
+function clr() {
+    document.getElementById("data").value = "";
+}
+//trigger enter key
+var input = document.getElementById("data");
+input.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        document.getElementById("solve").click();
+    }
+});
 
